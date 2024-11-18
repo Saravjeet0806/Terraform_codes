6a600042c1f2a2f6fd3c90f8ded4158e9fc69ac5
Sarav <Saravjeetofficial@gmail.com>
2024-11-18 23:57:03 +0530

    modified main.tf

--- AWS/Create Custom VPC and Launch EC2 instance using Terraform/main.tf
+++ AWS/Create Custom VPC and Launch EC2 instance using Terraform/main.tf
@@ -1,16 +1,18 @@
 provider "aws" {
-  region = "us-east-1" # Change as per your requirement
+  region = "us-east-1" 
 }
 
+# VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block           = "172.36.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "MyVPC"
+    Name = "MyVPC_TF"
   }
 }
 
+# Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.36.1.0/24"
@@ -20,6 +22,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+# Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.36.2.0/24"
@@ -28,6 +31,7 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.my_vpc.id
   tags = {
@@ -35,6 +39,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Public Route Table
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.my_vpc.id
 
@@ -48,14 +53,17 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+# Associate Public Subnet with Route Table
 resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
+# Security Group for EC2
 resource "aws_security_group" "ec2_sg" {
   vpc_id = aws_vpc.my_vpc.id
 
+  # Allow SSH (port 22)
   ingress {
     from_port   = 22
     to_port     = 22
@@ -63,6 +71,7 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow HTTP (port 80)
   ingress {
     from_port   = 80
     to_port     = 80
@@ -70,6 +79,7 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow HTTPS (port 443)
   ingress {
     from_port   = 443
     to_port     = 443
@@ -77,6 +87,7 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow all egress
   egress {
     from_port   = 0
     to_port     = 0
@@ -89,10 +100,10 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
+# EC2 Instance
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0866a3c8686eaeeba"
+  ami           = "ami-0866a3c8686eaeeba" # Update this to your desired AMI
   instance_type = "t2.micro"
-
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
@@ -101,18 +112,3 @@ resource "aws_instance" "my_ec2" {
   }
 }
 
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
-}
-
-output "ec2_instance_id" {
-  value = aws_instance.my_ec2.id
-}
