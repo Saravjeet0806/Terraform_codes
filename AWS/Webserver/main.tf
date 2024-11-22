e3aa19c586e30e0302bb1ef97f986b47e733dd89
Sarav <Saravjeetofficial@gmail.com>
2024-11-22 15:28:44 +0530

    webserver using terraform in aws

--- AWS/Webserver/main.tf
+++ AWS/Webserver/main.tf
@@ -1,9 +1,7 @@
-# Configure the AWS provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create a Security Group for an EC2 instance
 resource "aws_security_group" "instance" {
   name        = "terraform-example-instance"
   description = "Allow HTTP traffic on port 8080"
@@ -23,27 +21,25 @@ resource "aws_security_group" "instance" {
   }
 }
 
-# Create an EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-0866a3c8686eaeeba" # Ensure this AMI is valid 
   instance_type = "t2.micro"
 
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum install -y wget
-    mkdir -p /var/www/html
-    wget -O /var/www/html/index.html https://saravjeet0806.github.io/calculator-app-reactjs/
-    nohup busybox httpd -f -p 8080 &
-  EOF
+  key_name = "sarav_server" # Use the uploaded key pair
+
+ user_data = <<-EOF
+	      #!/bin/bash
+	      echo "Hello, World" > index.html
+	      nohup busybox httpd -f -p 8080 &
+	      EOF
 
   tags = {
     Name = "terraform-example"
   }
 }
 
-# Output variable: Public IP address
 output "public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.example.public_ip
